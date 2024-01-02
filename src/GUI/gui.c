7a1a169459485b0c5a9b1f6f6400329a755edc42
Lena <vincennes.lena@gmail.com>
2024-01-02 15:31:42 +0100

    Fix addProfile

--- src/GUI/gui.c
+++ src/GUI/gui.c
@@ -1,59 +1,58 @@
 #include "../../includes/define.h"
 
+static GtkBuilder *globalBuilder = NULL;
+static int current_profile_id = 1;
+
+
 static GtkWidget *globalProfileSubmenu = NULL;
-static GtkWidget *globalSessionSubmenu = NULL;
-static GtkWidget *globalConfigurationSubmenu = NULL;
 static gboolean is_profile_submenu_created = FALSE;
-static gboolean is_session_submenu_created = FALSE;
-static gboolean is_configuration_submenu_created = FALSE;
 
+static GtkWidget *addProfileWindow = NULL;
+static GtkEntry *entry = NULL;
+gboolean enterPressed = FALSE;
 
-// Fonction pour fermer les sous-menus s'ils sont ouverts
 static void close_submenus() {
     if (is_profile_submenu_created) {
         gtk_widget_destroy(globalProfileSubmenu);
         is_profile_submenu_created = FALSE;
     }
-
-    if (is_session_submenu_created) {
-        gtk_widget_destroy(globalSessionSubmenu);
-        is_session_submenu_created = FALSE;
-    }
-
-    if (is_configuration_submenu_created) {
-        gtk_widget_destroy(globalConfigurationSubmenu);
-        is_configuration_submenu_created = FALSE;
-    }
 }
 
-// Gestionnaire d'événements pour fermer les sous-menus lorsqu'un clic est effectué ailleurs
 static gboolean on_window_button_press_event() {
     close_submenus();
     return FALSE;
 }
 
-void on_profile_activate(GtkWidget *widget, gpointer user_data) {
-    // Fermer les sous-menus s'ils sont ouverts
-    close_submenus();
+void on_profile_menu_item_activate(GtkMenuItem *menu_item, gpointer user_data) {
+    int user_id = GPOINTER_TO_INT(g_object_get_data(G_OBJECT(menu_item), "user_id"));
+    current_profile_id = user_id;
 
+    globalBuilder = gtk_builder_new_from_file("index.glade");
+
+    // Appelez les fonctions avec l'ID du profil
+    arraySessions(user_id);
+    arrayConfigurations(user_id);
+}
+
+void on_profile_activate(GtkWidget *widget, gpointer user_data) {
+    close_submenus();
     Profile *profiles = get_profiles();
 
-    // Si le sous-menu n'a pas été créé, créez-le
     if (!is_profile_submenu_created) {
         globalProfileSubmenu = gtk_menu_new();
 
         for (int i = 0; profiles[i].id != -1; ++i) {
             GtkWidget *profile_item = gtk_menu_item_new_with_label(profiles[i].username);
+            g_object_set_data(G_OBJECT(profile_item), "user_id", GINT_TO_POINTER(profiles[i].id));
+            g_signal_connect(profile_item, "activate", G_CALLBACK(on_profile_menu_item_activate), user_data);
             gtk_menu_shell_append(GTK_MENU_SHELL(globalProfileSubmenu), profile_item);
             gtk_widget_show(profile_item);
         }
 
-        // Ajouter un séparateur
         GtkWidget *separator = gtk_separator_menu_item_new();
         gtk_menu_shell_append(GTK_MENU_SHELL(globalProfileSubmenu), separator);
         gtk_widget_show(separator);
 
-        // Ajouter "Add a Profile"
         GtkWidget *add_profile_item = gtk_menu_item_new_with_label("Add a profile");
         gtk_menu_shell_append(GTK_MENU_SHELL(globalProfileSubmenu), add_profile_item);
         g_signal_connect(add_profile_item, "activate", G_CALLBACK(on_add_profile_activate), user_data);
@@ -67,111 +66,67 @@ void on_profile_activate(GtkWidget *widget, gpointer user_data) {
     gtk_menu_popup_at_pointer(GTK_MENU(globalProfileSubmenu), NULL);
 }
 
-void on_add_profile_activate() {
-    GtkBuilder *builder = gtk_builder_new_from_file("index.glade");
-    GtkWidget *addProfileWindow = GTK_WIDGET(gtk_builder_get_object(builder, "addProfile"));
+void on_add_profile_activate(GtkWidget *widget, gpointer user_data) {
 
-    if (addProfileWindow != NULL) {
-        gtk_widget_show_all(addProfileWindow);
-
-        // Récupérer l'entrée de l'utilisateur
-        GtkEntry *entry = GTK_ENTRY(gtk_builder_get_object(builder, "Addprofileinput"));
-
-        // Vérifier si l'objet entry est non nul
-        if (entry != NULL) {
-            // Connecter le signal "activate" du champ d'entrée à la fonction de création de profil
-            g_signal_connect(entry, "activate", G_CALLBACK(on_create_profile_clicked), NULL);
-        } else {
-            g_warning("Objet entry est NULL.");
+    if (addProfileWindow == NULL) {
+        addProfileWindow = GTK_WIDGET(gtk_builder_get_object(globalBuilder, "addProfile"));
+        if (entry == NULL) {
+            entry = GTK_ENTRY(gtk_builder_get_object(globalBuilder, "Addprofileinput"));
         }
-    } else {
-        g_warning("Impossible de charger la fenetre addProfile depuis le fichier Glade.");
+        gtk_entry_set_text(GTK_ENTRY(entry), "");
+        g_signal_connect(entry, "activate", G_CALLBACK(on_activate_entry), NULL);
+        g_signal_connect(addProfileWindow, "delete-event", G_CALLBACK(on_delete_event), NULL);
     }
 
-    g_object_unref(builder);
+    gtk_widget_show_all(addProfileWindow);
+}
+
+
+void on_activate_entry() {
+    enterPressed = TRUE;
+    on_create_profile_clicked(NULL, NULL);
+}
+
+gboolean on_delete_event() {
+    if (enterPressed) {
+        enterPressed = FALSE;
+        return FALSE;
+    } else {
+        gtk_widget_hide(addProfileWindow);
+        entry = NULL;
+        addProfileWindow = NULL;
+        return TRUE;
+    }
 }
 
 void on_create_profile_clicked(GtkWidget *widget, gpointer user_data) {
 
-    GtkEntry *entry = GTK_ENTRY(widget);
     const gchar *username = gtk_entry_get_text(entry);
-
     char *username_copy = g_strdup(username);
 
     create_profile(username_copy);
-
     g_free(username_copy);
 
-    GtkWidget *addProfileWindow = gtk_widget_get_toplevel(GTK_WIDGET(widget));
-    if (GTK_IS_WIDGET(addProfileWindow)) {
-        gtk_widget_destroy(addProfileWindow);
-    }
+    gtk_widget_hide(addProfileWindow);
+    entry = NULL;
+    addProfileWindow = NULL;
+    enterPressed = FALSE;
 }
 
-
-void on_session_activate(GtkWidget *widget, gpointer user_data) {
-    // Fermer les sous-menus s'ils sont ouverts
-    close_submenus();
-
-    Session *sessions = get_sessions_by_profile_id(1);
-
-    // Si le sous-menu n'a pas été créé, créez-le
-    if (!is_session_submenu_created) {
-        globalSessionSubmenu = gtk_menu_new();
-
-        for (int i = 0; sessions[i].id != -1; ++i) {
-            GtkWidget *session_item = gtk_menu_item_new_with_label(sessions[i].name);
-            gtk_menu_shell_append(GTK_MENU_SHELL(globalSessionSubmenu), session_item);
-            gtk_widget_show(session_item);
-        }
-
-        is_session_submenu_created = TRUE;
-    }
-
-    free(sessions);
-
-    gtk_menu_popup_at_pointer(GTK_MENU(globalSessionSubmenu), NULL);
-}
-
-void on_configuration_activate(GtkWidget *widget, gpointer user_data) {
-    // Fermer les sous-menus s'ils sont ouverts
-    close_submenus();
-
-    Configuration *configuration = get_configurations(1);
-
-    // Si le sous-menu n'a pas été créé, créez-le
-    if (!is_configuration_submenu_created) {
-        globalConfigurationSubmenu = gtk_menu_new();
-
-        for (int i = 0; configuration[i].id != -1; ++i) {
-            GtkWidget *configuration_item = gtk_menu_item_new_with_label(configuration[i].name);
-            gtk_menu_shell_append(GTK_MENU_SHELL(globalConfigurationSubmenu), configuration_item);
-            gtk_widget_show(configuration_item);
-        }
-
-        is_configuration_submenu_created = TRUE;
-    }
-
-    free(configuration);
-
-    gtk_menu_popup_at_pointer(GTK_MENU(globalConfigurationSubmenu), NULL);
-}
-
-void arraySession(GtkBuilder *builder) {
+void arraySessions(int profile_id) {
     GtkWidget *viewportsession;
-    GtkWidget *label;
     GtkWidget *button;
 
-    viewportsession = GTK_WIDGET(gtk_builder_get_object(builder, "viewportsession"));
+    viewportsession = GTK_WIDGET(gtk_builder_get_object(globalBuilder, "viewportsession"));
 
     // Créer un GtkGrid pour contenir le label et les boutons de session
     GtkWidget *grid = gtk_grid_new();
     gtk_grid_set_column_homogeneous(GTK_GRID(grid), TRUE);
-    Session *sessions = get_sessions_by_profile_id(1);
+    Session *sessions = get_sessions_by_profile_id(profile_id);
 
     int row = 1;  // Commencer à la deuxième ligne pour les boutons
 
-    for (int i = 0; sessions[i].id != -1; ++i) {  // Utilisez votre propre logique pour les sessions
+    for (int i = 0; sessions[i].id != -1; ++i) {
         // Créer un bouton avec le nom de la session
         button = gtk_button_new_with_label(sessions[i].name);
 
@@ -189,23 +144,23 @@ void arraySession(GtkBuilder *builder) {
     gtk_container_add(GTK_CONTAINER(viewportsession), grid);
 
     gtk_widget_show_all(viewportsession);  // Afficher tous les widgets
+    free(sessions);
 }
 
-void arrayConfigurations(GtkBuilder *builder) {
+void arrayConfigurations(int profile_id) {
     GtkWidget *viewportconfiguration;
     GtkWidget *button;
 
-    viewportconfiguration = GTK_WIDGET(gtk_builder_get_object(builder, "viewportconfiguration"));
+    viewportconfiguration = GTK_WIDGET(gtk_builder_get_object(globalBuilder, "viewportconfiguration"));
 
     // Créer un GtkGrid pour contenir le label et les boutons de session
     GtkWidget *grid = gtk_grid_new();
     gtk_grid_set_column_homogeneous(GTK_GRID(grid), TRUE);
-    Configuration *configurations = get_configurations(1);
-
+    Configuration *configurations = get_configurations(profile_id);
 
     int row = 1;  // Commencer à la deuxième ligne pour les boutons
 
-    for (int i = 0; configurations[i].id != -1; ++i) {  // Utilisez votre propre logique pour les configurations
+    for (int i = 0; configurations[i].id != -1; ++i) {
         // Créer un bouton avec le nom de la session
         button = gtk_button_new_with_label(configurations[i].name);
 
@@ -223,18 +178,19 @@ void arrayConfigurations(GtkBuilder *builder) {
     gtk_container_add(GTK_CONTAINER(viewportconfiguration), grid);
 
     gtk_widget_show_all(viewportconfiguration);  // Afficher tous les widgets
+    free(configurations);
 }
 
+void destroyWindow(GtkWidget *widget, gpointer user_data){
+    g_object_unref(globalBuilder);
+}
 void activate(GtkApplication *app, gpointer user_data) {
     GtkWidget *window;
     GtkWidget *profile;
-    GtkWidget *session;
-    GtkWidget *configuration;
-    GtkBuilder *builder;
 
-    builder = gtk_builder_new_from_file("index.glade");
+    globalBuilder = gtk_builder_new_from_file("index.glade");
 
-    window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
+    window = GTK_WIDGET(gtk_builder_get_object(globalBuilder, "window"));
 
     GError *error = NULL;
     GdkPixbuf *icon = gdk_pixbuf_new_from_file("car.png", &error);
@@ -249,20 +205,16 @@ void activate(GtkApplication *app, gpointer user_data) {
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), user_data);
     g_signal_connect(window, "button-press-event", G_CALLBACK(on_window_button_press_event), NULL);
 
-    gtk_builder_connect_signals(builder, user_data);
+    gtk_builder_connect_signals(globalBuilder, user_data);
 
-    profile = GTK_WIDGET(gtk_builder_get_object(builder, "profile"));
-    session = GTK_WIDGET(gtk_builder_get_object(builder, "sessions"));
-    configuration = GTK_WIDGET(gtk_builder_get_object(builder, "configuration"));
-    arraySession(builder);
-    arrayConfigurations(builder);
+    profile = GTK_WIDGET(gtk_builder_get_object(globalBuilder, "profile"));
+
+    arraySessions(current_profile_id);
+    arrayConfigurations(current_profile_id);
 
     g_signal_connect(profile, "button_press_event", G_CALLBACK(on_profile_activate), window);
-    g_signal_connect(session, "button_press_event", G_CALLBACK(on_session_activate), window);
-    g_signal_connect(configuration, "button_press_event", G_CALLBACK(on_configuration_activate), window);
-
-    g_object_unref(builder);
 
     gtk_widget_show_all(window);
     gtk_main();
+
 }
