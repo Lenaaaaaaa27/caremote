8bc1f5946fded8f4e08ae58712acf86c1a9aff7b
Arthur <arthur.morelon@gmail.com>
2024-01-11 15:29:08 +0100

    fix

--- src/control/control.c
+++ src/control/control.c
@@ -11,7 +11,7 @@
 #define VAR_AXE_X 15
 
 #define TIME_LOOP 50
-#define VMAXPULSE (configuration->speed_step*255)/100
+#define VMAXPULSE ((configuration->speed_step*255)/100)
 
 int control(Configuration *configuration, int id_user, int clientSocket){
     int axe_X = MIN_AXE_X;
