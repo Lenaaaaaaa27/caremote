6ec6f7a53c2a5c3f75a5612e42a232a99e746121
Arthur <arthur.morelon@gmail.com>
2024-01-18 17:29:56 +0100

    Add directory selection

--- src/curl/curl.c
+++ src/curl/curl.c
@@ -1,47 +1,66 @@
-#include <stdio.h>
-#include <curl/curl.h>
+#include "../../includes/define.h"
 
 size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
     return fwrite(ptr, size, nmemb, stream);
 }
 
 int curl() {
+
+    char currentDir[MAX_PATH];
+    getcwd(currentDir, sizeof(currentDir));
+
     CURL *curl = curl_easy_init();
     if (!curl) {
-        fprintf(stderr, "Erreur lors de l'initialisation de cURL.\n");
-        return 1;
+        return EXIT_FAILURE;
     }
-
-    FILE *fp = fopen("caremote.zip", "wb");
-    if (!fp) {
-        fprintf(stderr, "Erreur lors de l'ouverture du fichier de sortie.\n");
-        curl_easy_cleanup(curl);
-        return 1;
-    }
-
-    const char *owner = "fantom609";
-    const char *repo = "caremote_car";
-    char url[256];
-    snprintf(url, sizeof(url), "https://github.com/%s/%s/archive/master.zip", owner, repo);
-
     curl_easy_setopt(curl, CURLOPT_CAINFO, "curl-ca-bundle.crt");
-    curl_easy_setopt(curl, CURLOPT_URL, url);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
     curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
 
-    CURLcode res = curl_easy_perform(curl);
+    OPENFILENAME ofn;
+    char filePath[MAX_PATH] = "caremote_car.zip";
+    ZeroMemory(&ofn, sizeof(ofn));
+    ofn.lStructSize = sizeof(ofn);
+    ofn.hwndOwner = NULL;
+    ofn.lpstrFilter = "Fichiers ZIP (*.zip)\0*.zip\0";
+    ofn.lpstrFile = filePath;
+    ofn.nMaxFile = MAX_PATH;
+    ofn.lpstrTitle = "Enregistrer le fichier ZIP";
+    ofn.Flags = OFN_OVERWRITEPROMPT;
 
-    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+    if (GetSaveFileName(&ofn) != 0) {
+        FILE *file = fopen(filePath, "wb");
+        if (!file) {
+            chdir(currentDir);
+            error_content(301);
+            return EXIT_FAILURE;
+        }
+        chdir(currentDir);
+        const char *owner = "fantom609";
+        const char *repo = "caremote_car";
+        char url[256];
+        snprintf(url, sizeof(url), "https://github.com/%s/%s/archive/master.zip", owner, repo);
 
-    if (res != CURLE_OK) {
-        fprintf(stderr, "curl_easy_perform() a échoué : %s\n", curl_easy_strerror(res));
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, file);
+
+
+        CURLcode res = curl_easy_perform(curl);
+
+        curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+
+        if (res != CURLE_OK) {
+            fprintf(stderr, "curl_easy_perform() a échoué : %s\n", curl_easy_strerror(res));
+            return EXIT_FAILURE;
+        }
+
+        fclose(file);
+
     }
-
     curl_easy_cleanup(curl);
-    fclose(fp);
 
     curl_global_cleanup();
 
-    return 0;
+
+    return EXIT_SUCCESS;
 }
